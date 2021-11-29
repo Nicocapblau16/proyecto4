df490ea948b26f2f1defdc3158d6a3b6bd8b82a6
Nicolau Guardiola <nico.guardiolat@gmail.com>
2021-11-29 13:43:26 +0100

    a

--- Assets/script/GameManager.cs
+++ Assets/script/GameManager.cs
@@ -1,42 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+using TMPro;
+
 
 public class GameManager : MonoBehaviour
 {
     public bool gameOver;
     public GameObject[] targetPrefabs;
     public Vector3 randomSpawnPos;
+    public List<Vector3> targetPosition;
+    public TextMeshProUGUI scoreText;
 
     private float minX = -3.75f;
     private float minY = -3.75f;
     private float spaceBetweenSquares = 2.5f;
     private int numberRows = 4;
     private float spawnRate = 2f;
+    private int score = 0;
 
     private void Start()
     {
         StartCoroutine("SpawnRandomTarget");
+        score = 0;
+    }
+
+    private void Update()
+    {
+        scoreText.text = $"Score: {score}";
     }
 
     public Vector3 RandomSpawnPosition()
     {
         // genera pos aleatoria en uno de los centros de los 16 cuadrados
-        int randomInt = Random.Range(0, numberRows);
-        float randomPosX = minX + randomInt * spaceBetweenSquares;
-        float randomPosY = minY + randomInt * spaceBetweenSquares;
+        int randomIntX = Random.Range(0, numberRows);
+        int randomIntY = Random.Range(0, numberRows);
+        float randomPosX = minX + randomIntX * spaceBetweenSquares;
+        float randomPosY = minY + randomIntY * spaceBetweenSquares;
 
         return new Vector3(randomPosX, randomPosY, 0);
     }
 
     private IEnumerator SpawnRandomTarget()
     {
-        yield return new WaitForSeconds(spawnRate);
+        while (!gameOver)
+        {
+            yield return new WaitForSeconds(spawnRate);
 
-        int randomInedx = Random.Range(0, targetPrefabs.Length);
-        randomSpawnPos = RandomSpawnPosition();
+            int randomInedx = Random.Range(0, targetPrefabs.Length);
+            randomSpawnPos = RandomSpawnPosition();
 
-        Instantiate(targetPrefabs[randomInedx], randomSpawnPos, targetPrefabs[randomInedx].transform.rotation);
+            while (targetPosition.Contains(randomSpawnPos))
+            {
+                randomSpawnPos = RandomSpawnPosition();
+            }
+
+            Instantiate(targetPrefabs[randomInedx], randomSpawnPos, targetPrefabs[randomInedx].transform.rotation);
+            targetPosition.Add(randomSpawnPos);
+        }
+        
     }
 
 }
--- Assets/script/target.cs
+++ Assets/script/target.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class target : MonoBehaviour
 {
     private float timeDestroy = 2f;
+    private GameManager gameManagerScript;
 
     void Start()
     {
         Destroy(gameObject, timeDestroy);
+
+        gameManagerScript = FindObjectOfType<GameManager>();
     }
 
 
@@ -17,13 +20,20 @@ public class target : MonoBehaviour
         if (gameObject.CompareTag("Good"))
         {
             Destroy(gameObject);
+            
 
         }
 
         else if (gameObject.CompareTag("Bad"))
         {
             Destroy(gameObject);
+            
         }
         
+    }
+
+    private void OnDestroy()
+    {
+        gameManagerScript.targetPosition.Remove(transform.position);
     }
 }
